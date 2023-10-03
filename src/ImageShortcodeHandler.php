c5373d9fbb809aeb0157ec85a5f2f34cd9bfebd5
Loz Calver <lozcalver@bigfork.co.uk>
2023-10-03 15:08:21 +0100

    Update shortcode with a bunch of upstream changes

--- src/ImageShortcodeHandler.php
+++ src/ImageShortcodeHandler.php
@@ -2,11 +2,15 @@
 
 namespace Bigfork\HTMLEditorSrcset;
 
+use Psr\SimpleCache\CacheInterface;
+use SilverStripe\Assets\File;
 use SilverStripe\Assets\Image;
 use SilverStripe\Assets\Shortcodes\ImageShortcodeProvider;
+use SilverStripe\Assets\Storage\AssetStore;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Convert;
+use SilverStripe\Core\Injector\Injector;
 
 class ImageShortcodeHandler
 {
@@ -25,7 +29,7 @@ class ImageShortcodeHandler
     private static $pixel_densities = [];
 
     /**
-     * Most of this is copied straight from Image::handle_shortcode()
+     * Most of this is copied straight from ImageShortcodeProvider
      *
      * Replace"[image id=n]" shortcode with an image reference.
      * Permission checks will be enforced by the file routing itself.
@@ -39,17 +43,32 @@ class ImageShortcodeHandler
      */
     public static function handle_shortcode($args, $content, $parser, $shortcode, $extra = array())
     {
-        // Find appropriate record, with fallback for error handlers
-        $record = ImageShortcodeProvider::find_shortcode_record($args);
+        $cache = ImageShortcodeProvider::getCache();
+        $cacheKey = ImageShortcodeProvider::getCacheKey($args, $content);
+        $cachedMarkup = static::getCachedMarkup($cache, $cacheKey, $args);
+        if ($cachedMarkup) {
+            return $cachedMarkup;
+        }
 
+        // Find appropriate record, with fallback for error handlers
+        $fileFound = true;
+        $record = ImageShortcodeProvider::find_shortcode_record($args, $errorCode);
+        if ($errorCode) {
+            $fileFound = false;
+            $record = static::find_error_record($errorCode);
+        }
         if (!$record) {
             return null; // There were no suitable matches at all.
         }
 
+        // Check if a resize is required
+        $width = null;
+        $height = null;
+        $grant = static::getGrant($record);
+        $src = $record->getURL($grant);
+
         $srcsetSources = [];
 
-        // Check if a resize is required
-        $src = $record->Link();
         if ($record instanceof Image) {
             $width = isset($args['width']) ? intval($args['width']) : null;
             $height = isset($args['height']) ? intval($args['height']) : null;
@@ -79,26 +98,93 @@ class ImageShortcodeHandler
             }
         }
 
+        // Determine whether loading="lazy" is set
+        $args = self::updateLoadingValue($args, $width, $height);
+
         // Build the HTML tag
         $attrs = array_merge(
-            // Set overrideable defaults
-            ['src' => '', 'alt' => $record->Title],
+        // Set overrideable defaults
+            ['src' => '', 'alt' => ''],
             // Use all other shortcode arguments
             $args,
             // But enforce some values
             ['id' => '', 'src' => $src, 'srcset' => implode(', ', $srcsetSources)]
         );
 
-        // Clean out any empty attributes
-        $attrs = array_filter($attrs, function ($v) {
-            return (bool)$v;
-        });
+        // If file was not found then use the Title value from static::find_error_record() for the alt attr
+        if (!$fileFound) {
+            $attrs['alt'] = $record->Title;
+        }
 
-        // Condense to HTML attribute string
-        $attrsStr = join(' ', array_map(function ($name) use ($attrs) {
-            return Convert::raw2att($name) . '="' . Convert::raw2att($attrs[$name]) . '"';
-        }, array_keys($attrs)));
+        if ($record instanceof Image) {
+            $record->extend('updateAttributes', $attrs);
+        }
 
-        return '<img ' . $attrsStr . ' />';
+        // Clean out any empty attributes (aside from alt) and anything not whitelisted
+        $whitelist = array_merge(ImageShortcodeProvider::config()->get('attribute_whitelist'), ['srcset']);
+        $attrs = array_filter($attrs ?? [], function ($v, $k) use ($whitelist) {
+            return in_array($k, $whitelist) && (strlen(trim($v ?? '')) || $k === 'alt');
+        }, ARRAY_FILTER_USE_BOTH);
+
+        $markup = ImageShortcodeProvider::createImageTag($attrs);
+
+        // Cache it for future reference
+        if ($fileFound) {
+            $cache->set($cacheKey, [
+                'markup' => $markup,
+                'filename' => $record instanceof File ? $record->getFilename() : null,
+                'hash' => $record instanceof File ? $record->getHash() : null,
+            ]);
+        }
+
+        return $markup;
+    }
+
+    private static function updateLoadingValue(array $args, ?int $width, ?int $height): array
+    {
+        if (!Image::getLazyLoadingEnabled()) {
+            return $args;
+        }
+        if (isset($args['loading']) && $args['loading'] == 'eager') {
+            // per image override - unset the loading attribute unset to eager load (default browser behaviour)
+            unset($args['loading']);
+        } elseif ($width && $height) {
+            // width and height must be present to prevent content shifting
+            $args['loading'] = 'lazy';
+        }
+        return $args;
+    }
+
+    protected static function getCachedMarkup($cache, $cacheKey, $arguments): string
+    {
+        $item = $cache->get($cacheKey);
+        $assetStore = Injector::inst()->get(AssetStore::class);
+        if ($item && $item['markup'] && !empty($item['filename'])) {
+            // Initiate a protected asset grant if necessary
+            $allowSessionGrant = static::getGrant(null, $arguments);
+            if ($allowSessionGrant && $assetStore->exists($item['filename'], $item['hash'])) {
+                $assetStore->grant($item['filename'], $item['hash']);
+                return $item['markup'];
+            }
+        }
+        return '';
+    }
+
+    protected static function getGrant(?File $record, ?array $args = null): bool
+    {
+        $grant = ImageShortcodeProvider::config()->allow_session_grant;
+        ImageShortcodeProvider::singleton()->extend('updateGrant', $grant, $record, $args);
+        return $grant;
+    }
+
+    protected static function find_error_record($errorCode)
+    {
+        $result = ImageShortcodeProvider::singleton()->invokeWithExtensions('getErrorRecordFor', $errorCode);
+        $result = array_filter($result ?? []);
+        if ($result) {
+            return reset($result);
+        }
+
+        return null;
     }
 }
